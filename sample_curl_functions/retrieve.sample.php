f4de7972a6e2561b2d5bb9cfec4e47d0cbb00ff1
Kevin Jan <kevinjanbarluado2@gmail.com>
2022-03-09 09:40:21 +0800

    added sample_curl_functions

---
+++ sample_curl_functions/retrieve.sample.php
@@ -0,0 +1,24 @@
+<?php
+//API URL
+$url = 'http://localhost/codeigniter/api/example/user/';
+
+//API key
+$apiKey = 'CODEX@123';
+
+//Auth credentials
+$username = "admin";
+$password = "1234";
+
+//create a new cURL resource
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array("X-API-KEY: " . $apiKey));
+curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+
+$result = curl_exec($ch);
+
+//close cURL resource
+curl_close($ch);
